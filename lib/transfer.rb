1af490a4c16606318e4107d1f3c400a9c1019331
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-07 01:54:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,8 +12,14 @@ def valid?
   sender.valid? && receiver.valid?
 end
 
-def execute_transaction
-  
-end
+def execute_transaction(sender, receiver, amount)
+      if valid? && sender.balance > sender.amount && sender.staus == "pending"
+       sender.balance -= amount
+        @receiver.balance += @amount
+        @status = "complete"
+       else @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
 
 end
